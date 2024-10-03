e5c5ec694f9cd0db06993710425e7698ce72dd29
zzzlichzzz <lonedarkwolf.09@gmail.com>
2024-10-03 08:50:24 +0300

    Комментарии
    Добавил комментарии

--- src/main/java/com/lset/atom/common/blocks/BlockMixing.java
+++ src/main/java/com/lset/atom/common/blocks/BlockMixing.java
@@ -7,15 +7,21 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 public class BlockMixing {
+    // Метод для проверки соседних блоков и выполнения смешивания
     public static void tryMixBlocks(World world, BlockPos pos){
+        // Получаем блок в текущей позиции
         BlockState blockState = world.getBlockState(pos);
 
+        // Проверка соседних блоков
         for (Direction direction : Direction.values()){
             BlockPos neighborPos = pos.offset(direction);
             BlockState neighborBlockState = world.getBlockState(neighborPos);
 
+            // Пример: если рядом находится песок и глина, создаем новый блок
             if (blockState.isOf(Blocks.SAND) && neighborBlockState.isOf(Blocks.CLAY)) {
+                //         Первый блок^                            Второй блок^
                 world.setBlockState(pos,Blocks.GREEN_CONCRETE.getDefaultState());
+                //         блок при смешивании^
             }
         }
     }
--- src/main/java/com/lset/atom/mixin/MixingBlockMixins.java
+++ src/main/java/com/lset/atom/mixin/MixingBlockMixins.java
@@ -11,8 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(World.class)
 public class MixingBlockMixins {
+
+    // Вставляем код при добавлении нового блока в мир
     @Inject(method = "setBlockState",at = @At("RETURN"))
     private void onBlockSet(BlockPos pos, BlockState state, int flags, CallbackInfoReturnable<Boolean>cir){
+        // Вызов вашего метода для проверки соседних блоков и смешивания
         BlockMixing.tryMixBlocks((World) (Object)this,pos);
     }
 
